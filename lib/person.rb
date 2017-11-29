49f9229b3cad9c9b0bba11d9047f06e452ec3ce3
Josh Kramer <funemployeddad@gmail.com>
2017-11-29 21:09:15 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+  def name(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def jobs(job)
+    @jobs = job
+  end
+
+  def jobs
+    @jobs
+
+  def jobs=(new_job)
+    @jobs = new_job
+  end
+end
